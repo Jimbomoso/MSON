b2719296eab889527afad0d77bc18dc649b4bd55
jimbomoso <jimbomoso@gmail.com>
2020-04-08 15:09:53 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -12,11 +12,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 app.use('/session', routes.session);
 app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
     req.context = {
         models,
         me: await models.User.findByLogin('admin'),
